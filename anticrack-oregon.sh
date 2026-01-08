6fa8dd05dd85501ac05f505cea8381fade7c5c86
Chase Bryan <196585384+chbryan@users.noreply.github.com>
2026-01-08 05:08:31 -0600

    Add script to block Oregon domains and IPs
    This script enables and disables blocking of specific domains and IP ranges related to Oregon. It also manages backups of the hosts file and iptables.

---
+++ anticrack-oregon.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DOMAINS_URL="https://raw.githubusercontent.com/blocklistproject/Lists/master/scam.txt"
+HOSTS_FILE="/etc/hosts"
+HOSTS_BACKUP="/etc/hosts.bak"
+IPTABLES_BACKUP="/etc/iptables.bak"
+
+function enable() {
+  cp $HOSTS_FILE $HOSTS_BACKUP
+  iptables-save > $IPTABLES_BACKUP
+
+  # Download and block domains
+  curl -s $DOMAINS_URL | grep -v '^#' >> $HOSTS_FILE
+
+  # Block IP ranges (IPv4 only)
+  IPS="159.121.0.0/16 167.131.0.0/16 170.104.0.0/16 199.48.32.0/20 205.235.192.0/20 198.176.0.0/21"
+  for ip in $IPS; do
+    iptables -A INPUT -s $ip -j DROP
+    iptables -A OUTPUT -d $ip -j DROP
+  done
+
+  # Remove related packages
+  apt purge -y oregon* 2>/dev/null
+
+  # Set non-default DNS
+  echo "nameserver 9.9.9.9" > /etc/resolv.conf
+
+  echo "Oregon blocked."
+}
+
+function disable() {
+  cp $HOSTS_BACKUP $HOSTS_FILE
+  iptables-restore < $IPTABLES_BACKUP
+  echo "nameserver 8.8.8.8" > /etc/resolv.conf  # Restore example
+  echo "Oregon unblocked."
+}
+
+case $1 in
+  enable) enable ;;
+  disable) disable ;;
+  *) echo "Usage: $0 enable|disable" ;;
+esac
